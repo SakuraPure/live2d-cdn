828404ac5e422c39c4a9f1342120b891d20c60ca
SakuraPure <xulinxuan1225@gmail.com>
2020-08-24 20:50:16 +0800

    update

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "https://www.peko233.ga/live2d/";
-const tips_path = "https://www.peko233.ga/live2d/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/sakurapure/live2d-cdn@1.1/live2d/";
+const tips_path = "https://cdn.jsdelivr.net/gh/sakurapure/live2d-cdn@1.1/live2d/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
@@ -34,8 +34,8 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "https://www.peko233.ga/live2d_api/",
-			//cdnPath: "https://你的CDN域名/live2d_api/"从本地加载此项不用管
+			apiPath: "https://cdn.jsdelivr.net/gh/sakurapure/live2d-cdn@1.1/live2d_api/",
+			cdnPath: "https://cdn.jsdelivr.net/gh/sakurapure/live2d-cdn@1.1/live2d_api/
 		});
 	});
 console.log(`
